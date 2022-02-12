498dae1b820549aa117203f03beaae09d05bbed1
Graham <graham.lim88@gmail.com>
2022-02-12 11:41:40 +0800

    base modal in-class done

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 console.log('hi from script.js');
 
-const showHideDescription = (btnId) => {
+const openDescription = (btnId) => {
   // change id to "div-xx"
   // we are slicing to get the characters after the
   const divId = `div-${btnId.slice(4)}`;
@@ -9,15 +9,33 @@ const showHideDescription = (btnId) => {
 
   if (divToHide.classList.contains('hide')) {
     divToHide.classList.remove('hide');
-  } else {
-    divToHide.classList.add('hide');
   }
 };
 
-const btnArray = document.querySelectorAll('button');
+const closeDescription = (btnId) => {
+  // change id to "div-xx"
+  // we are slicing to get the characters after the
+  const divId = `div-${btnId.slice(6)}`;
+  console.log(divId);
+  const divToHide = document.querySelector(`#${divId}`);
+
+  divToHide.classList.add('hide');
+};
+
+const btnArray = document.querySelectorAll('.open'); // search for all buttons w class "open"
+console.log(btnArray);
 btnArray.forEach((btn) => {
   btn.addEventListener('click', (event) => {
     console.log(event.currentTarget.id);
-    showHideDescription(event.currentTarget.id); // id is "btn-xx"
+    openDescription(event.currentTarget.id); // id is "btn-xx"
+  });
+});
+
+const btnArray2 = document.querySelectorAll('.close'); // search for all buttons w class "close"
+console.log(btnArray2);
+btnArray2.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    console.log(event.currentTarget.id);
+    closeDescription(event.currentTarget.id); // id is "close-xx"
   });
 });
